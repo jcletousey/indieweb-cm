54dcfee0ba4d473388c1da986188dffd1ab9264a
Jean-Charles Le Tousey <jcltousey@gmail.com>
2024-02-06 15:15:13 +0100

    Manage changes for Git

--- src/lib/server/utils.js
+++ src/lib/server/utils.js
@@ -1,3 +1,5 @@
+import { encode } from 'js-base64';
+
 export const getItemsPerLocale = (entries) => {
   const items = {};
   for (const [key, value] of entries) {
@@ -18,3 +20,83 @@ export const getItemsPerLocale = (entries) => {
   }
   return items;
 };
+
+export const defineCommitChanges = (type, items, getFileContent) => {
+  const changes = new Array();
+  for (const locale in items) {
+    const path = `src/content/${locale}/${type}/${getFileName(items[locale]?.title)}`;
+    const content = getFileContent(items[locale]);
+    const encodedContent = encode(content);
+    changes.push({ path: path, contents: encodedContent });
+  }
+  return changes;
+};
+
+export const slugify = (str) => {
+  return String(str)
+    .normalize('NFKD') // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .trim() // trim leading or trailing whitespace
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
+    .replace(/\s+/g, '-') // replace spaces with hyphens
+    .replace(/-+/g, '-'); // remove consecutive hyphens
+};
+
+// Helper function to clean strings for frontmatter
+export const sanitize = (str) => {
+  // replace endash and emdash with hyphens
+  str = str.replace(/–/g, '-');
+  str = str.replace(/—/g, '-');
+
+  // replace double quotes and apostrophes
+  str = str.replace(/"/g, "'");
+  str = str.replace(/“/g, "'");
+  str = str.replace(/”/g, "'");
+  str = str.replace(/’/g, "'");
+
+  return str.trim();
+};
+
+// generate the frontmatter string
+export const getFrontmatter = (yaml) => {
+  let fm = new Array();
+  fm.push('---');
+  Object.keys(yaml).forEach((key) => {
+    if (yaml[key] && yaml[key].constructor == String) {
+      fm.push(`${key}: ${yaml[key]}`);
+    } else if (typeof yaml[key] === 'boolean') {
+      fm.push(`${key}: ${String(yaml[key])}`);
+    }
+  });
+  fm.push('---');
+  return fm.join('\n');
+};
+
+// generate the new md file name
+export const getFileName = (title) => {
+  const date = new Date();
+  const unixSeconds = date.getTime();
+  let filename = formatDate(date);
+
+  if (!title) {
+    filename = `${filename}/${unixSeconds}`;
+  } else {
+    const slug = slugify(title);
+    filename += slug.length > 1 ? `/${slug}` : `/${unixSeconds}`;
+  }
+
+  return `${filename}.md`;
+};
+
+/** https://bobbyhadz.com/blog/javascript-format-date-dd-mm-yyyy#format-a-date-as-ddmmyyyy-in-javascript */
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function formatDate(date) {
+  return [date.getFullYear(), padTo2Digits(date.getMonth() + 1), padTo2Digits(date.getDate())].join(
+    '/',
+  );
+}
--- src/routes/bookmark/+page.server.js
+++ src/routes/bookmark/+page.server.js
@@ -1,9 +1,36 @@
-import { getItemsPerLocale } from '$lib/server/utils';
+import {
+  getItemsPerLocale,
+  getFrontmatter,
+  sanitize,
+  defineCommitChanges,
+} from '$lib/server/utils';
+
+// generate the new md file content
+const getFileContent = (data) => {
+  const { title, link, link_locale, body, tags } = data;
+  const date = new Date().toISOString();
+
+  const frontMatter = getFrontmatter({
+    title: `"${sanitize(title)}"`,
+    date: `"${date}"`,
+    lang: `"${link_locale}"`,
+    link: `"${link}"`,
+    tags: `"${tags}"`,
+  });
+
+  let content = frontMatter;
+  if (body) {
+    content += '\n\n' + sanitize(body);
+  }
+
+  return content;
+};
 
 /** @type {import('./$types').Actions} */
 export const actions = {
   default: async ({ request }) => {
     const formData = await request.formData();
     const bookmarks = getItemsPerLocale(formData.entries());
+    const changes = defineCommitChanges('bookmarks', bookmarks, getFileContent);
   },
 };
